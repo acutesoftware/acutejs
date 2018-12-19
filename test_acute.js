ec53b03385694922072c34162374f2e64b802f41
acutesoftware <djmurray@acutesoftware.com.au>
2018-12-19 23:49:29 +1030

    test for domAddText

--- test_acute.js
+++ test_acute.js
@@ -1,3 +1,12 @@
 function runTests() {
-  alert('running unit tests from test_acute.js')
+  alert('running unit tests from test_acute.js');
+  //let res = '05';
+  //let  lPad('5', 2) = '05'
+
+  if True {
+    domAddText('lpad test passed');
+  }
+  else {
+    domAddText('lpad test failed');
+  }
 }
