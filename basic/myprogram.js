1ffc5b2d1562153ca1eb3f2f52a5b4bb7e9507ba
acutesoftware <djmurray@acutesoftware.com.au>
2019-06-10 14:50:57 +0930

    basic tutorial on intro to javascript

---
+++ basic/myprogram.js
@@ -0,0 +1,26 @@
+// myprogram.js
+
+//init()
+print("Hello World")
+
+border("green", "blue")
+
+happyFace(100,100, 75)
+normalFace(300,100, 50)
+sadFace(500,100, 30)
+
+
+
+function myFunction1() {
+    print('you pressed button 1')
+    // you can add more code here that runs when button 1 is pressed
+
+    event.preventDefault()
+}
+
+function myFunction2() {
+    print('you pressed button 2')
+    // you can add more code here that runs when button 2 is pressed
+
+    event.preventDefault()
+}
